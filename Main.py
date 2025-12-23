dc69bafd5d978ef8f6aff785222e00bc43ebc8fc
coport-uni <ohsungwoo@unist.ac.kr>
2025-12-23 09:18:28 +0900

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,141 +1,43 @@
-<<<<<<< HEAD
-import sys
+from PyArduino import PyArduino 
 import time
-from telemetrix_uno_r4.wifi.telemetrix_uno_r4_wifi import telemetrix_uno_r4_wifi
-import logging
 
-class PyArduino():
-    def __init__(self):
-        self.board = telemetrix_uno_r4_wifi.TelemetrixUnoR4WiFi(transport_type=1)
+class AILeveling():
+    def __init__(self, board_type : str):
+        """
+        This function initialize board as board_type. User must input wifi, minima
 
-    def run_digital_write(self, pin_number : int, pin_state : bool):
-        self.board.set_pin_mode_digital_output(pin_number)
-
-        if pin_state is True:
-            self.board.digital_write(pin_number, 1)
-            logging.info(f'{pin_number} is {pin_state}')
-
-        elif pin_state is False:
-            self.board.digital_write(pin_number, 0)
-            logging.info(f'{pin_number} is {pin_state}')
-
-        time.sleep(0.001)
-
-        # self.board.shutdown()
-        
-    def get_analog_state(self, pin_number : int):
-        global analog_value
-        analog_value = None
-        self.board.set_pin_mode_analog_input(pin_number, differential = 0, callback = self.get_state_slicer)
-
-        while analog_value is None:
-            time.sleep(0.01)
-
-        # self.board.shutdown()
-
-        return analog_value
-
-    def get_state_slicer(self, data):
-        global analog_value
-        pin_mode_index = 0
-        pin_number_index = 1
-        pin_state_index = 2
-
-        # print(data[pin_state_index])
-
-        analog_value = data[pin_state_index]
+        Input : Str
+        Output : None
+        """
+        self.pa = PyArduino(board_type)
     
+    def run_example(self):
+        """
+        This function command arduino with digital input or analog output
+
+        Input : None
+        Output : None
+        """
+        pin = 7
+        self.pa.run_digital_write(pin, True)
+        time.sleep(2)
+        self.pa.run_digital_write(pin, False)
+        time.sleep(2)
+
+        # for i in range(4):
+        #     self.pa.run_digital_write(i+4, True)
+        #     time.sleep(2)
+        #     self.pa.run_digital_write(i+4, False)
+        #     time.sleep(2)
+
+        # value = pa.get_analog_state(2)
+        # print(value)
+
 def main():
-    pa = PyArduino()
-    # 센서 예제
-    # value = pa.get_analog_state(2)
-    # print(value)
+    while True:
+        al = AILeveling("minima")
+        al.run_test()
 
-    # # 디지털 입출력 예제
-    # for pin_number in range(4,8):
-    #     pa.run_digital_write(pin_number, True)
-    #     time.sleep(1)
-    #     pa.run_digital_write(pin_number, False)
-
-    # 밸브, 외란 펌프 예제
-    pa.run_digital_write(4, True)
-    pa.run_digital_write(5, True)
-    
-    pa.run_digital_write(6, True)
-    pa.run_digital_write(7, True)
-
-
-if __name__ is '__main__':
+if __name__ == "__main__":
     main()
-
-
-
-            
-            
-
-=======
-import sys
-import time
-from telemetrix_uno_r4.wifi.telemetrix_uno_r4_wifi import telemetrix_uno_r4_wifi
-import logging
-
-class PyArduino():
-    def __init__(self):
-        self.board = telemetrix_uno_r4_wifi.TelemetrixUnoR4WiFi(transport_type=1)
-
-    def run_digital_write(self, pin_number : int, pin_state : bool):
-        self.board.set_pin_mode_digital_output(pin_number)
-
-        if pin_state is True:
-            self.board.digital_write(pin_number, 1)
-            logging.info(f'{pin_number} is {pin_state}')
-
-        elif pin_state is False:
-            self.board.digital_write(pin_number, 0)
-            logging.info(f'{pin_number} is {pin_state}')
-
-        time.sleep(0.001)
-
-        # self.board.shutdown()
         
-    def get_analog_state(self, pin_number : int):
-        global analog_value
-        analog_value = None
-        self.board.set_pin_mode_analog_input(pin_number, differential = 0, callback = self.get_state_slicer)
-
-        while analog_value is None:
-            time.sleep(0.01)
-
-        # self.board.shutdown()
-
-        return analog_value
-
-    def get_state_slicer(self, data):
-        global analog_value
-        pin_mode_index = 0
-        pin_number_index = 1
-        pin_state_index = 2
-
-        # print(data[pin_state_index])
-
-        analog_value = data[pin_state_index]
-    
-def main():
-    pa = PyArduino()
-    value = pa.get_analog_state(2)
-    print(value)
-
-    for pin_number in range(4,8):
-        pa.run_digital_write(pin_number, True)
-        time.sleep(1)
-        pa.run_digital_write(pin_number, False)
-
-if __name__ is '__main__':
-    main()
-
-
-
-            
-            
-
->>>>>>> 889aea2d9e4a65a27b8ad4d884df2dd088c5b590
